c091be3ca46062f189de24228be2e7954a39ba90
krzaq <krzaq@krzaq.cc>
2026-02-23 00:05:41 +0100

    Remove collapsible section fold functionality

--- src/components/CollapsibleSection.vue
+++ src/components/CollapsibleSection.vue
@@ -1,35 +1,15 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-
-const props = withDefaults(defineProps<{
+defineProps<{
   title: string
-  defaultOpen?: boolean
-}>(), {
-  defaultOpen: true,
-})
-
-const isOpen = ref(props.defaultOpen)
-
-function toggle() {
-  isOpen.value = !isOpen.value
-}
+}>()
 </script>
 
 <template>
   <section class="border border-gray-700 rounded-lg">
-    <button
-      type="button"
-      class="flex w-full items-center justify-between px-4 py-3 text-left font-semibold text-amber-50 hover:bg-gray-800/50"
-      :aria-expanded="isOpen"
-      @click="toggle"
-    >
-      <span>{{ title }}</span>
-      <span
-        class="text-xs transition-transform duration-150"
-        :class="isOpen ? 'rotate-90' : ''"
-      >&#9654;</span>
-    </button>
-    <div v-show="isOpen" class="px-4 pb-4">
+    <div class="px-4 py-3 font-semibold text-amber-50">
+      {{ title }}
+    </div>
+    <div class="px-4 pb-4">
       <slot />
     </div>
   </section>
